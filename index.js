3e675c02f74866b72cce4922253e3e9a5fb834ee
Harry Fielder <harry@harryfielder.com>
2021-02-02 22:11:37 +0000

    updated port to process env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const dotenv = require('dotenv').config();
 
 
